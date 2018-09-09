724998ed93f64a154fa2fcebeb129323b87f99f6
Alex Yakunin <alex.yakunin@gmail.com>
2018-09-08 20:53:50 -0700

    Speed test: duration -> 5s -> 1ms, pass count: 1 -> 10 (+ select max. throughput)

--- src/CSharp/Main/SpeedTest/SpeedTester.cs
+++ src/CSharp/Main/SpeedTest/SpeedTester.cs
@@ -10,7 +10,8 @@ namespace GCBurn.SpeedTest
 {
     public class SpeedTester
     {
-        public static TimeSpan DefaultDuration = TimeSpan.FromSeconds(5);
+        public const int PassCount = 10;
+        public static TimeSpan DefaultDuration = TimeSpan.FromMilliseconds(1);
         public TimeSpan Duration = DefaultDuration;
         public int ThreadCount = Environment.ProcessorCount;
         public IndentedTextWriter Writer = new IndentedTextWriter(Console.Out, "  ");
@@ -23,38 +24,37 @@ namespace GCBurn.SpeedTest
         
         public void Run()
         {
-            GC.Collect();
             var duration = Duration.TotalSeconds;
-
             using (Writer.Section($"Test settings:")) {
-                Writer.AppendMetric("Duration", duration, "s");
+                Writer.AppendMetric("Duration", duration * 1000, "ms");
                 Writer.AppendMetric("Thread count", ThreadCount, "");
                 Writer.AppendMetric("Unit size", UnitAllocator.UnitSize, "B");
             }
-            
-            var allocators = Enumerable.Range(0, ThreadCount)
-                .Select(i => new UnitAllocator())
-                .ToArray();
-            var threads = allocators
-                .Select(a => new Thread(() => a.Run(Duration)))
-                .ToArray();
-
-            foreach (var thread in threads)
-                thread.Start();
-            foreach (var thread in threads)
-                thread.Join();
-
             Writer.AppendLine();
 
-            // Computing thread & globalpauses
-            var count = allocators.Sum(a => a.AllocationCount);
-            var size = count * UnitAllocator.UnitSize;
-            var fullSize = count * (UnitAllocator.UnitSize + GarbageAllocator.ObjectSize);
+            var totalCount = 0L;
+            for (var pass = 0; pass < PassCount; pass++) {
+                var allocators = Enumerable.Range(0, ThreadCount)
+                    .Select(i => new UnitAllocator())
+                    .ToArray();
+                var threads = allocators
+                    .Select(a => new Thread(() => a.Run(Duration)))
+                    .ToArray();
 
+                GC.Collect();
+                foreach (var thread in threads)
+                    thread.Start();
+                foreach (var thread in threads)
+                    thread.Join();
+                totalCount = Math.Max(totalCount, allocators.Sum(a => a.AllocationCount));
+            } 
+            
+            var totalSize = totalCount * UnitAllocator.UnitSize;
+            var totalSizeWithOverhead = totalCount * (UnitAllocator.UnitSize + GarbageAllocator.ObjectSize);
             using (Writer.Section($"Allocation speed:")) {
-                Writer.AppendMetric("Operations per second", count / Sizes.Mega / duration, "M/s");
-                Writer.AppendMetric("Bytes per second", size  / duration / Sizes.GB, "GB/s");
-                Writer.AppendMetric("Bytes per second (incl. overhead)", fullSize  / duration / Sizes.GB, "GB/s");
+                Writer.AppendMetric("Operations per second", totalCount / Sizes.Mega / duration, "M/s");
+                Writer.AppendMetric("Bytes per second", totalSize  / duration / Sizes.GB, "GB/s");
+                Writer.AppendMetric("Bytes per second (incl. overhead)", totalSizeWithOverhead  / duration / Sizes.GB, "GB/s");
             }
             Writer.AppendLine();
         }
--- src/Go/speed_test/speed_tester.go
+++ src/Go/speed_test/speed_tester.go
@@ -7,7 +7,8 @@ import (
 	"time"
 )
 
-var DefaultDuration = time.Duration(5 * time.Second)
+const PassCount = 10
+var DefaultDuration = time.Duration(time.Millisecond)
 
 type SpeedTester struct {
 	NoOutput        bool
@@ -32,9 +33,7 @@ func NewWarmupSpeedTester() *SpeedTester {
 }
 
 func (t *SpeedTester) Run() {
-	runtime.GC()
 	duration := t.Duration.Seconds()
-
 	if !t.NoOutput {
 		fmt.Printf("Test settings:\n")
 		fmt.Printf("  Duration:     %v s\n", int(duration))
@@ -42,45 +41,41 @@ func (t *SpeedTester) Run() {
 		fmt.Println()
 	}
 
-	var done = make(chan bool)
-	var allocators []*UnitAllocator
-	for i := 0; i < t.ThreadCount; i++ {
-		allocators = append(allocators, NewUnitAllocator())
-	}
+	totalCount := int64(0)
+	for pass := 0; pass < PassCount; pass++  {
+		var done = make(chan bool)
+		var allocators []*UnitAllocator
+		for i := 0; i < t.ThreadCount; i++ {
+			allocators = append(allocators, NewUnitAllocator())
+		}
 
-	memStatsBefore := new(runtime.MemStats)
-	runtime.ReadMemStats(memStatsBefore)
+		runtime.GC()
+		// Doing this step separately to make sure we Run this at almost the same time
+		for _, a := range allocators {
+			go a.Run(t.Duration, done)
+		}
+		// Wait for goroutines to complete
+		for range allocators {
+			<-done
+		}
 
-	// Doing this step separately to make sure we Run this at almost the same time
-	for _, a := range allocators {
-		go a.Run(t.Duration, done)
+		tc := int64(0)
+		for _, a := range allocators {
+			tc += a.AllocationCount
+		}
+		if totalCount < tc {
+			totalCount = tc
+		}
 	}
-	// Wait for goroutines to complete
-	for range allocators {
-		<-done
-	}
-
-	memStatsAfter := new(runtime.MemStats)
-	runtime.ReadMemStats(memStatsAfter)
 
 	if t.NoOutput {
 		return
 	}
 
-	totalCount := int64(0)
-	for _, a := range allocators {
-		totalCount += a.AllocationCount
-	}
 	totalSize := totalCount * UnitSize
 
 	fmt.Printf("Allocation speed:\n")
 	fmt.Printf("  Operations per second: %.2f M/s\n", float64(totalCount)/duration/Mega)
 	fmt.Printf("  Bytes per second:      %.2f GB/s\n", float64(totalSize)/duration/GB)
-	fmt.Println()
-	fmt.Printf("Runtime GC stats:\n")
-	fmt.Printf("  RAM used:              %.3f -> %.3f GB\n", float64(memStatsBefore.Alloc)/GB, float64(memStatsAfter.Alloc)/GB)
-	fmt.Printf("  GC rate:               %.3f /s\n", float64(memStatsAfter.NumGC-memStatsBefore.NumGC)/duration)
-	fmt.Printf("  Allocation rate:       %.3f GB/s\n", float64(memStatsAfter.TotalAlloc-memStatsBefore.TotalAlloc)/duration/GB)
-	fmt.Printf("  Free rate:             %.3f GB/s\n", float64(memStatsAfter.Frees-memStatsBefore.Frees)/duration/GB)
 	fmt.Println()
 }
